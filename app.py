e2a2c08b34b10a4cdd7970186d465d0d2f62b43d
Joke Heyndels <jo_hannah@outlook.com>
2019-01-14 21:18:56 +0100

    added database, created story/stories templates for testing

--- app.py
+++ app.py
@@ -1,8 +1,14 @@
 import os
 from flask import Flask, app, redirect, url_for, render_template, request, flash
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
+from config import mongo_uri, mongodb
 
 app = Flask(__name__)
+app.config["MONGO_DBNAME"] = mongodb()
+app.config["MONGO_URI"] = mongo_uri()
 
+mongo = PyMongo(app)
 
 @app.route('/')
 def index():
@@ -24,14 +30,21 @@ def profile(user):
     return render_template("index.html")
 
 
+@app.route('/all_stories')
+def all_stories():
+    return render_template("allstories.html", 
+    stories=mongo.db.stories.find())
+
+
 @app.route('/search')
 def search():
     return render_template("index.html")
 
 
-@app.route('/<title>')
-def read(title):
-    return render_template("index.html")
+@app.route('/story')
+def story():
+    return render_template("story.html", 
+    stories=mongo.db.stories.find())
 
 
 @app.route('/new_story')
