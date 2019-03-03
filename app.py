284a7d2b29557d3c9075b08f6b929b20cd6e394a
Joke Heyndels <jo_hannah@outlook.com>
2019-03-03 21:25:19 +0100

    set up environment variables instead of using config file

--- app.py
+++ app.py
@@ -7,9 +7,9 @@ from config import mongo_uri, mongodb, secret_key, test_user
 from slugify import slugify
 
 app = Flask(__name__)
-app.config["MONGO_DBNAME"] = mongodb()
-app.config["MONGO_URI"] = mongo_uri()
-app.config["SECRET_KEY"] = secret_key()
+app.config["MONGO_DBNAME"] = os.getenv('MONGO_DBNAME')
+app.config["MONGO_URI"] = os.getenv('MONGO_URI')
+app.config["SECRET_KEY"] = os.getenv('SECRET_KEY')
 
 mongo = PyMongo(app)
 
